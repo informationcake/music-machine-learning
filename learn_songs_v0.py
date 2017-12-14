cbbbec69ac3a9a2408f849b2ad8ee6e2ad59a5ea
Alex Clarke <informationcake@gmail.com>
2017-12-14 17:31:55 +0000

    Update learn_songs_v0.py

--- learn_songs_v0.py
+++ learn_songs_v0.py
@@ -100,3 +100,72 @@ names=np.unique(Y_test)
 # Inputs to train classifiers are: features_train, artists_train
 # Inputs to test classifiers are: features_test, artists_test
 # Some classifiers will require classes labeled as numbers; if so, use Y_train_n, Y_test_n, X_test, X_train
+
+#CODE BELOW IS JUST AN EXAMPLE, YOU WILL NEED TO EDIT IT A LOT DEPENDING ON WHAT YOU WANT TO DO
+
+
+
+
+#investigate log-loss? Compare to dummy_classifier? gradient boosted forest? hyper-parms in your fav classifier? Confusion matrix for multi-class?
+
+
+#e.g. lets do a simple random forest
+forest=RandomForestClassifier(n_estimators=25, random_state=1, class_weight='balanced')
+forest.fit(feature_train, artists_train)
+predicted_artists=forest.predict(features_test)
+print(Classification_report(artists_test,predicted_artists))
+
+#Support Vector MAchines (SVM) need numerical classes:
+svm=svm.SVC(class_weight='balanced')
+svm.fit(X_train, Y_train_n)
+
+#MPL? basic neural network. NEEDS normalised data. 
+from sklearn.preprocessing import StandardScaler
+scaler=StandardScaler()
+scaler.fit(features_train)
+nn=MPLClassifier(hidden_layer_sizes=(i,), solver='adam', max_iter=2000)
+#nn.fit()
+#etc etc
+
+
+
+#how many trees are needed? check out cross_validating stuff:
+scores=[]
+for trees in range(1,10):
+    forest=RandomForestClassifier(n_estimators=25, random_state=1, class_weight='balanced')
+    validated=cross_validate(forest, X, Y, cv=5, scoring=['f1_weighted'])
+    score.append(validated)
+
+
+    
+    
+#Rank features and remove unimportant ones?
+genres_pred=forest.predict(features_test)
+accuracy_before=accuracy_score(artists_test, artists_pred)
+print('accuracy with all features included: {0}'.format(accuracy_before))
+# get importances and standard deviations, plot them in order of importance
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+             axis=0)
+indices = np.argsort(importances)[::-1]
+print("Feature ranking:")
+feature_names_importanceorder=[]
+for f in range(len(indices)):
+    #print("%d. feature %d (%f) {0}" % (f + 1, indices[f], importances[indices[f]]), feature_names[indices[f]])
+    feature_names_importanceorder.append(str(feature_names[indices[f]]).rstrip("'][").strip("[]'"))
+
+# plot the feature importances of the forest
+plt.figure()
+plt.title("Feature importances before pruning")
+plt.bar(range(len(indices)), importances[indices],
+       color="r", yerr=std[indices], align="center")
+plt.xticks(range(len(indices)), indices)
+plt.xlim([-1, len(indices)])
+plt.xticks(range(len(indices)), feature_names_importanceorder, rotation='vertical')
+plt.tight_layout()
+plt.show()
+
+
+
+
+
